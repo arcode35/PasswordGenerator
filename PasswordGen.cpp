a7825fa7c3602adcded5a542e9ae75f11838d6ae
cppcoder <45514615+xcppcoder@users.noreply.github.com>
2022-10-07 22:22:15 -0500

    Add files via upload

---
+++ PasswordGen.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+#include <ctime>
+
+#include <vector>
+
+int main() {
+    srand(time(NULL));
+    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVQXYZabcdefghijklmnopqrstuvwxyz";
+    std::string numbers = "123456789";
+    std::string symbols = "!@#$%^&*()<>?:";
+
+    std::string password;
+
+    int num;
+
+    std::string final = "";
+
+    char response;
+
+    int length;
+
+    std::cout << "Do you want your password to have characters? (y/n): ";
+    std::cin >> response;
+    (response == 'y') ? (final += alphabet) : final += "";
+
+    std::cout << "Do you want your password to have numbers? (y/n): ";
+    std::cin >> response;
+    (response == 'y') ? (final += numbers) : final += "";
+
+    std::cout << "Do you want your password to have symbols? (y/n): ";
+    std::cin >> response;
+
+    std::cout << "How long do you want your password to be? (y/n): ";
+    std::cin >> length;
+
+    (response == 'y') ? (final += symbols) : final += "";
+
+    std::cout << "How many passwords would you like to generate? (y/n): ";
+    std::cin >> num;
+
+    std::vector < std::string > passList;
+
+    for (int j = 1; j <= num; j++) {
+
+        for (int i = 0; i < length; i++) {
+            int randNum;
+
+            randNum = std::rand() % final.length();
+
+            password += final[randNum];
+
+            if (password.length() == length) {
+                passList.push_back(password);
+                password = "";
+            }
+        }
+
+    }
+    int j = 1;
+    for (auto a : passList) {
+
+        std::cout << j << ": " << a << "\n";
+        j++;
+    }
+
+}
